44707faf5db69eca38bb95149bbb52b8beb3d794
Ping9 <sim2626@naver.com>
2025-04-06 03:17:04 +0900

    docker 설정

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,22 @@
-FROM python:3.9-slim
+FROM python:3.12-slim
 
 WORKDIR /app
 
+RUN mkdir -p ./db_data
+
+# 시스템 패키지 설치
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# wait-for-postgres.sh 복사 및 실행 권한 부여
+COPY wait-for-postgres.sh /app/wait-for-postgres.sh
+RUN chmod +x /app/wait-for-postgres.sh
+
+# Python 의존성 설치
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
+# 앱 코드 복사
 COPY . .
-
-CMD ["uvicorn", "app.Main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/config/database.py
+++ app/config/database.py
@@ -1,10 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:qwer1234@localhost/examdb"
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
+
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
