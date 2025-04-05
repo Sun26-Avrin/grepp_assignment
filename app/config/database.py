1c4c92a57d47b945cb2c00239b8281dcd4ff46b4
Ping9 <sim2626@naver.com>
2025-04-06 03:18:44 +0900

    Revert "docker 설정"
    This reverts commit 44707faf5db69eca38bb95149bbb52b8beb3d794.

--- app/config/database.py
+++ app/config/database.py
@@ -1,18 +1,10 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:qwer1234@localhost/examdb"
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
-
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
